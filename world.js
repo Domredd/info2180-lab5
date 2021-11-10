18cac4be5cb7ba9ee3b06f72e3196b42a89131a6
Domredd <reddicks32@gmail.com>
2021-11-09 23:22:56 -0500

    Done Exercises

---
+++ world.js
@@ -0,0 +1,24 @@
+window.onload = function (){
+    let btn = document.getElementById("lookup");
+    let btn_cities = document.getElementById("Clookup");
+    let result = document.getElementById("result");
+    let text = document.getElementById("country");
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () =>{
+        let output = xhr.responseText;
+        result.innerHTML = output;
+    });
+
+    btn.addEventListener("click", () => {
+        let url = 'world.php?country='+text.value;
+        xhr.open('GET',url);
+        xhr.send();
+    });
+
+    btn_cities.addEventListener("click", () => {
+        let url = 'world.php?country='+input.value+'&context=cities';
+        xhr.open('GET',url);
+        xhr.send();
+    });
+}
